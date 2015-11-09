40c02bc3418c4b0ad779020e55ad53b1a77cb165
Ryan Douglas <ryan@luckydogdigital.com>
2015-11-08 18:24:02 -0700

    having problems pushing comments into dom. May be narrowing it down

--- app.js
+++ app.js
@@ -1,21 +1,57 @@
 $(document).ready(function() {
 
 console.log("doc ready");
-  var $newElem = function (title, thumb) {
-    // console.log("new elem function");
-    $('.main-content').append('<div class="row"><div class="article"><div class="media"><div class="media-left media-middle"><a href="#"><img class="media-object" src="'+ thumb + '" alt="..."></a></div><div class="media-body"><h4 class="media-heading">'+title+'</h4></div></div></div></div>');
+  var $newElem = function (title, thumb, comment) {
+    // console.log(title);
+    // console.log(thumb);
+    // console.log(comment);
+    $('.main-content').append('<div class="row"><div class="article panel-default"><div class="media"><div class="container col-md-3"><div class="media-left media-middle"><a href="#"><img class="media-object" src="'+thumb+'" alt="..."></a></div></div><div class="container col-md-9"><div class="media-body"><h3 class="media-heading">' + title + '</h3></div><div class="row"><div class="media-body"><button type="button" class="succinct" name="succint">succinct</button><button type="button" name="button">comments</button><button type="button" name="button">original thread</button></div></div><div class="row start-hidden"> <p>'+ comment +'</div></div></div></div></div>');
   };
 
-  var $newElemNoThumb = function (title) {
-    // console.log("new elem function");
-    $('.main-content').append('<div class="row"><div class="article"><div class="media"><div class="media-left media-middle"><a href="#"><img class="media-object" src="http://placecreature.com/nicolas-cage/150/150" alt="..."></a></div><div class="media-body"><h4 class="media-heading">'+title+'</h4></div></div></div></div>');
+  var $newElemNoThumb = function (title, comment) {
+    // console.log(title);
+    // console.log(comment);
+    $('.main-content').append('<div class="row"><div class="article panel-default"><div class="media"><div class="container col-md-3"><div class="media-left media-middle"><a href="#"><img class="media-object" src="http://placecreature.com/nicolas-cage/150/150" alt="..."></a></div></div><div class="container col-md-9"><div class="media-body"><h3 class="media-heading">' + title + '</h3></div><div class="row"><div class="media-body"><button type="button" class="succinct" name="succint">succinct</button><button type="button" name="button">comments</button><button type="button" name="button">original thread</button></div></div><div class="row start-hidden">' + comment + '</div></div></div></div></div>');
+
+  };
+
+  var testFunction = function () {
+    return "test line";
   };
 
   var $getComments = function ($relLink) {
+    var longestComment = 'test';
     $.get('https://www.reddit.com/' + $relLink + '.json', function (data) {
-      // console.log('get comments');
-      // console.log(data);
+      console.log(data);
+      var commentObj = data[1];
+      // console.log("commentObj = ");
+      // console.log(commentObj);
+      var thread = commentObj.data.children;
+      // console.log('thread = ');
+      // console.log(thread);
+
+
+
+      for (var i = 0; i < 10; i++) {
+        var comment = [];
+        if (thread[i].data.body !== undefined) {
+          comment = thread[i].data.body;
+          // console.log('comment = ');
+          // console.log(comment);
+        }
+        if (comment.length > longestComment.length) {
+          longestComment = comment;
+        }
+      }
+
+
+      // console.log(longestComment);
+      // console.log('longest comment = ');
+      // console.log(longestComment);
+
     });
+    console.log(longestComment);
+    return "TEST PASS";
   };
 
   var getRequests = function () {
@@ -25,46 +61,33 @@ console.log("doc ready");
       // console.log('done');
       // console.log(data);
 
-      // for (var i=0; i<10; i+=1) {
-      //   var sublisting = data.data.children[i];
-      //   if (sublisting.data.domain !== "self.science"){
-      //     console.log(sublisting.data.title, sublisting.data.thumbnail, sublisting.data.permalink);
-      //     var $title = sublisting.data.title;
-      //     var $thumb = sublisting.data.thumbnail;
-      //     var $relLink = sublisting.data.permalink;
-      //     var $formatLink = $relLink.substring(0, $relLink.length - 1);
-      //
-      //     console.log("formatted link: " + $formatLink);
-      //     console.log("get request : https://www.reddit.com/" + $formatLink + ".json");
-      //     $getComments($formatLink);
+      for (var i=0; i < 5; i+=1) {
+        var sublisting = data.data.children[i];
+        if (sublisting.data.domain !== "self.science"){
+          // console.log(sublisting.data.title, sublisting.data.thumbnail, sublisting.data.permalink);
+          var $title = sublisting.data.title;
+          var $thumb = sublisting.data.thumbnail;
+          var $relLink = sublisting.data.permalink;
+          var $formatLink = $relLink.substring(0, $relLink.length - 1);
 
-          // if (sublisting.data.thumbnail === 'default') {
-          //   $newElemNoThumb($title);
-          // } else {
-          //   $newElem($title, $thumb);
-          // }
-    //     }
-    //   }
-    //
+          // console.log("formatted link: " + $formatLink);
+          // console.log("get request : https://www.reddit.com/" + $formatLink + ".json");
+
+          var comment = $getComments($formatLink);
+
+          console.log(comment);
+
+          if (sublisting.data.thumbnail === 'default') {
+            $newElemNoThumb($title, comment );
+          } else {
+            $newElem($title, $thumb, comment );
+          }
+        }
+      }
+
     });
 
   };
-
-  $.get('https://www.reddit.com//r/science/comments/3rvwml/purdue_researchers_successfully_target_achilles.json', function (data) {
-    console.log(data);
-    var commentObj = data[1];
-    var thread = commentObj.data.children;
-    var longestComment = '';
-    for (var i = 0; i < thread.length; i++) {
-      var comment = thread[i].data.body;
-      // console.log(comment);
-      // console.log(comment.length);
-      if (comment.length > longestComment.length) {
-        longestComment = comment;
-      }
-    }
-    console.log(longestComment);
-  });
 
 
   getRequests();
@@ -74,12 +97,14 @@ console.log("doc ready");
   //       console.log(data);
   //     });
 
-  $('#succinct').on('click', function () {
+  $('body').on('click', '.succinct', function () {
+    console.log('succinct push');
     var panel = $(this).closest('.media').find('.start-hidden');
     if ($(panel).is(':hidden')) {
       console.log('hidden p');
       $(panel).slideDown("slow");
     } else {
+
       $(panel).slideUp("slow");
     }
   });
