bc456659d07fa4d1c9dd01e00578a2cff4c6ee3d
Ryan Douglas <ryan@luckydogdigital.com>
2015-11-08 21:21:40 -0700

    at a stopping point

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
 
 console.log("doc ready");
-  var $newElem = function (title, thumb, comment) {
+
+// Global Variables
+var longestComment = '';
+
+
+// Spare:
+// <div class="row start-hidden">'+ comment +'</div>
+
+  var $newElem = function (title, thumb, data) {
     // console.log(title);
     // console.log(thumb);
     // console.log(comment);
-    $('.main-content').append('<div class="row"><div class="article panel-default"><div class="media"><div class="container col-md-3"><div class="media-left media-middle"><a href="#"><img class="media-object" src="'+thumb+'" alt="..."></a></div></div><div class="container col-md-9"><div class="media-body"><h3 class="media-heading">' + title + '</h3></div><div class="row"><div class="media-body"><button type="button" class="succinct" name="succint">succinct</button><button type="button" name="button">comments</button><button type="button" name="button">original thread</button></div></div><div class="row start-hidden"> <p>'+ comment +'</div></div></div></div></div>');
+    $('.main-content').append('<div class="row"><div class="article panel-default"><div class="media"><div class="container col-md-3"><div class="media-left media-middle"><a href="#"><img class="media-object" src="'+thumb+'" alt="..."></a></div></div><div class="container col-md-9"><div class="media-body"><h3 class="media-heading">' + title + '</h3></div><div class="row"><div class="media-body"><button type="button" class="succinct" data="'+data+'" name="succint">succinct</button><button type="button" name="button">comments</button><button type="button" name="button">original thread</button></div></div></div></div></div></div>');
   };
 
-  var $newElemNoThumb = function (title, comment) {
+  var $newElemNoThumb = function (title, data) {
     // console.log(title);
     // console.log(comment);
-    $('.main-content').append('<div class="row"><div class="article panel-default"><div class="media"><div class="container col-md-3"><div class="media-left media-middle"><a href="#"><img class="media-object" src="http://placecreature.com/nicolas-cage/150/150" alt="..."></a></div></div><div class="container col-md-9"><div class="media-body"><h3 class="media-heading">' + title + '</h3></div><div class="row"><div class="media-body"><button type="button" class="succinct" name="succint">succinct</button><button type="button" name="button">comments</button><button type="button" name="button">original thread</button></div></div><div class="row start-hidden">' + comment + '</div></div></div></div></div>');
+    $('.main-content').append('<div class="row"><div class="article panel-default"><div class="media"><div class="container col-md-3"><div class="media-left media-middle"><a href="#"><img class="media-object" src="http://placecreature.com/nicolas-cage/150/150" alt="..."></a></div></div><div class="container col-md-9"><div class="media-body"><h3 class="media-heading">' + title + '</h3></div><div class="row"><div class="media-body"><button type="button" class="succinct" data = "'+ data +'" name="succint">succinct</button><button type="button" name="button">comments</button><button type="button" name="button">original thread</button></div></div></div></div></div></div>');
 
   };
 
@@ -28,7 +36,7 @@ console.log("doc ready");
       console.log('done');
       var commentObj = data[1];
       var thread = commentObj.data.children;
-      var longestComment = 'test';
+      // var longestComment = 'test';
 
 
       for (var i = 0; i < 10; i++) {
@@ -48,7 +56,7 @@ console.log("doc ready");
       }
       // console.log('longest comment: ');
       // console.log(longestComment);
-      return "TEST PASS";
+      // return "TEST PASS";
     });
     // console.log('longest comment: ');
     // console.log(longestComment);
@@ -60,28 +68,21 @@ console.log("doc ready");
     $.get('https://www.reddit.com/r/science.json',
     function (data) {
       // console.log('done');
-      // console.log(data);
+        console.log(data);
 
       for (var i=0; i < 5; i+=1) {
         var sublisting = data.data.children[i];
         if (sublisting.data.domain !== "self.science"){
-          // console.log(sublisting.data.title, sublisting.data.thumbnail, sublisting.data.permalink);
           var $title = sublisting.data.title;
           var $thumb = sublisting.data.thumbnail;
           var $relLink = sublisting.data.permalink;
           var $formatLink = $relLink.substring(0, $relLink.length - 1);
-
-          // console.log("formatted link: " + $formatLink);
-          // console.log("get request : https://www.reddit.com/" + $formatLink + ".json");
-
-          var comment = $getComments($formatLink);
-
-          console.log('getting comment: ', comment);
+          $getComments($formatLink);
 
           if (sublisting.data.thumbnail === 'default') {
-            $newElemNoThumb($title, comment );
+            $newElemNoThumb($title, $formatLink );
           } else {
-            $newElem($title, $thumb, comment );
+            $newElem($title, $thumb, $formatLink );
           }
         }
       }
@@ -98,16 +99,31 @@ console.log("doc ready");
   //       console.log(data);
   //     });
 
-  $('body').on('click', '.succinct', function () {
+  $('body').on('click', '.succinct', function (event) {
     // console.log('succinct push');
-    var panel = $(this).closest('.media').find('.start-hidden');
-    if ($(panel).is(':hidden')) {
-      // console.log('hidden p');
-      $(panel).slideDown("slow");
-    } else {
+    //
+    // console.log(event.target);
+    // console.log($(this));
 
-      $(panel).slideUp("slow");
+    console.log('it\'s clicking');
+    var parent = $(this).closest('.media');
+    var hiddenP = $(parent).find('.start-hidden');
+    console.log(hiddenP.length);
+    if (hiddenP.length < 1) {
+      console.log('first if');
+      console.log(hiddenP.is(':hidden'));
+      $(parent).append('<div class="row start-hidden"><p>testing</p</div>');
+      $(hiddenP).slideDown('slow');
+
     }
-  });
+    if ($(hiddenP).is(':hidden')) {
+      console.log('second if');
+      $(hiddenP).slideDown('slow');
+    } else {
+      console.log('else');
+      $(hiddenP).slideUp("slow");
+    }
+
+});
 
 });
